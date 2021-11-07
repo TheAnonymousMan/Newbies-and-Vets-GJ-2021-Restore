1a0044616092c7b26503ec1ab99df9bf751425be
MasterofChow <48959792+MasterofChow@users.noreply.github.com>
2021-11-06 17:23:46 -0700

    Update baseClass.cs

--- Assets/Scripts/baseClass.cs
+++ Assets/Scripts/baseClass.cs
@@ -23,38 +23,38 @@ namespace classes
     public class baseClass : MonoBehaviour
     {
         //change 'name' later to represent something better
-        public GameObject cube;
-        public GameObject circle;
-        void Start()
-        {
-        }
+        public GameObject kamikaze;
+        public GameObject oscillator;
+        public GameObject waitAndGo;
+
         void Update()   
         {
-            //The reason for the spawn will change
+            // The reason for the spawn will change
             if (Input.GetKeyDown("x"))
             {
+                // Currently changing the spawn size, easy to create as a constant
                 var bomber = new enemyC(30, Random.Range(1f, 2f));
-                Debug.Log(bomber.health);
+                // Debug.Log(bomber.health);
                 
-                GameObject temp = Instantiate(cube, new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f)), Quaternion.identity);
+                GameObject temp = Instantiate(kamikaze, new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f)), Quaternion.identity);
                 temp.transform.localScale = new Vector3(bomber.size, bomber.size, bomber.size);
             }
             
             if (Input.GetKeyDown("k"))
             {
-                var hoverEnemy = new enemyC(150, 10);
-                Debug.Log(hoverEnemy.health);
+                var hoverEnemy = new enemyC(150, 1);
+                // Debug.Log(hoverEnemy.health);
 
-                GameObject temp = Instantiate(circle, new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f)), Quaternion.identity);
+                GameObject temp = Instantiate(oscillator, new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f)), Quaternion.identity);
                 temp.transform.localScale = new Vector3(hoverEnemy.size, hoverEnemy.size, hoverEnemy.size);
             }
 
             if (Input.GetKeyDown("f"))
             {
-                var crazy = new enemyC(100, 10);
-                Debug.Log(crazy.health);
+                var crazy = new enemyC(100, 1);
+                // Debug.Log(crazy.health);
 
-                GameObject temp = Instantiate(circle, new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f)), Quaternion.identity);
+                GameObject temp = Instantiate(waitAndGo, new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f)), Quaternion.identity);
                 temp.transform.localScale = new Vector3(crazy.size, crazy.size, crazy.size);
             }
         }
