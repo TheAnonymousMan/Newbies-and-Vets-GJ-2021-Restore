1d1bccc20b1983b3f051cdbeb3f73a21a92e472a
DeathWish007 <rishiprakaash007@gmail.com>
2021-11-07 13:36:04 -0800

    Enemy 3 & 4

--- Assets/Scripts/Enemy3Controller.cs
+++ Assets/Scripts/Enemy3Controller.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Enemy3Controller : MonoBehaviour
 {
+    public GameObject bullet;
+    
+    public float primaryfirerate;
+    private float timebtwprimary;
+    
+    public Transform first;
+    public Transform second;
+    public Transform third;
+
+    public float rotationSpeed;
+    
+    public float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        timebtwprimary = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.RotateAround(transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+        if (timebtwprimary <= 0)
+        {
+            var temp1 = Instantiate(bullet, first.position, first.rotation);
+            temp1.GetComponent<Rigidbody2D>().AddRelativeForce(first.transform.up * speed * 20f, ForceMode2D.Force);
+            // temp1.GetComponent<EnemyBullet>().damage = 4f;
+            // temp1.GetComponent<EnemyBullet>()._tag = "Four";
+
+
+            // var temp2 = Instantiate(bullet, second.position, first.rotation);
+            // temp2.GetComponent<Rigidbody2D>().AddRelativeForce(second.transform.up * speed * 20f, ForceMode2D.Force);
+            //
+            // temp2.GetComponent<EnemyBullet>().damage = 4f;
+            // temp2.GetComponent<EnemyBullet>()._tag = "Four";
+            // var temp3 = Instantiate(bullet, third.position, first.rotation);
+            // temp3.GetComponent<Rigidbody2D>().AddRelativeForce(third.transform.up * speed * 20f, ForceMode2D.Force);
+
+            timebtwprimary = primaryfirerate;
+        }
+
+        else
+        {
+            timebtwprimary -= Time.deltaTime;
+        }
     }
 }
---
+++ Assets/Scripts/Enemy4Controller.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Enemy4Controller : MonoBehaviour
+{
+    public float firerate;
+    private float timebtwFirerate;
+    private Vector3 startpoint;
+    public float speed;
+    public float numberofBullets;
+
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startpoint = transform.position;
+        if (timebtwFirerate <= 0)
+        {
+            var bulpos = 360f / numberofBullets;
+            var angle = 0f;
+
+            for (var i = 0; i <= numberofBullets-1; i++)
+            {
+                var xdir = startpoint.x + Mathf.Sin((angle * Mathf.PI) / 180 );
+                var ydir = startpoint.y + Mathf.Cos((angle * Mathf.PI) / 180 );
+
+                var direction = new Vector3(xdir, ydir, 0f);
+                var move = (direction - startpoint).normalized * speed;
+                var temp = Instantiate(bullet, startpoint, transform.rotation);
+                temp.GetComponent<Rigidbody2D>().AddRelativeForce((new Vector3(move.x, move.y, 0f)) * 90f, ForceMode2D.Force);
+                // temp.GetComponent<EnemyBullet>().damage = 3f;
+                // temp.GetComponent<EnemyBullet>()._tag = "Five";
+
+                angle += bulpos;
+
+                timebtwFirerate = firerate;
+            }
+        }
+        else
+        {
+            timebtwFirerate -= Time.deltaTime;
+        }
+    }
+}
