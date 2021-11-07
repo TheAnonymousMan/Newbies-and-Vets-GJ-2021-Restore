54322628e536d1f96cd349432b85c4601a9beca7
MasterofChow <48959792+MasterofChow@users.noreply.github.com>
2021-11-06 20:46:45 -0700

    quick update

--- Assets/Scripts/randomMovement.cs
+++ Assets/Scripts/randomMovement.cs
@@ -5,45 +5,47 @@ using UnityEngine;
 public class randomMovement : MonoBehaviour
 {
 
-    public float speed = .00005f;
+    private float speed = 1f;
 
     // Update is called once per frame
     void Update()
     {
         int dec = Random.Range(1,5);
         // Debug.Log(dec);
-        Debug.Log(transform.position);
+        // Debug.Log(transform.position);
+        // Debug.Log(new Vector3((speed * Time.deltaTime), 0f, 0f));
+        // Debug.Log(transform.position);
         switch(dec)
         {
             case 1:
             // Debug.Log("1");
             for (int i = 0; i < 1000; i++)
-                transform.position += new Vector3((speed * Time.deltaTime)/100, 0f, 0f);                
+                transform.position = transform.position + new Vector3(speed*Time.deltaTime, 0f, 0f);                
             break;
 
             case 2:
             // Debug.Log("2");
             for (int i = 0; i < 1000; i++)
-                transform.position += new Vector3(-(speed * Time.deltaTime)/100, 0f, 0f);
+                transform.position = transform.position - new Vector3(speed*Time.deltaTime, 0f, 0f);
             break;
 
             case 3:
             // Debug.Log("3");
             for (int i = 0; i < 1000; i++)
-                transform.position += new Vector3(0f, (speed * Time.deltaTime)/100, 0f);
+                transform.position = transform.position + new Vector3(0f, speed*Time.deltaTime, 0f);
             break;
 
             case 4:
             // Debug.Log("4");6
             for (int i = 0; i < 1000; i++)
-                transform.position += new Vector3(0f, -(speed * Time.deltaTime)/100, 0f);
+                transform.position = transform.position - new Vector3(0f, speed*Time.deltaTime, 0f);
             break;
 
             default:
             break;
         }
-        Debug.Log(speed * Time.deltaTime);
-        Debug.Log(transform.position);
+        // Debug.Log(speed * Time.deltaTime);
+        // Debug.Log(transform.position);
         if (gameObject.transform.position.x <= -10)
         {
             // Debug.Log("changed");
